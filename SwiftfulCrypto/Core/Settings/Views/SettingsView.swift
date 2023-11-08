b252ce4fbc474880d01458d59b21abea0ffd00cc
h-ashraf <81376919+h-ashraf@users.noreply.github.com>
2023-11-08 23:13:22 +0000

    Add files via upload

---
+++ SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,80 @@
+//
+//  SettingsView.swift
+//  SwiftfulCrypto
+//
+//  Created by Nick Sarno on 5/14/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let githubURL = URL(string: "https://github.com/h-ashraf")!
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // background
+                Color.theme.background
+                    .ignoresSafeArea()
+                
+                // content
+                List {
+                    swiftfulThinkingSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    applicationSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                }
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Information")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton()
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+extension SettingsView {
+    
+    private var swiftfulThinkingSection: some View {
+        Section(header: Text("Crypto Tracker")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made by Hamad Ashraf. It uses MVVM Architecture, Combine, and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("View my Github 🥳", destination: githubURL)
+        }
+    }
+    
+   
+    
+    private var applicationSection: some View {
+        Section(header: Text("Application")) {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+    
+    
+}
