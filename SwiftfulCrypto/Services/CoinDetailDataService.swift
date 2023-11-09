0d0927cb4ac48b0c1dc41097d487e3eea90792e7
h-ashraf <81376919+h-ashraf@users.noreply.github.com>
2023-11-09 00:07:55 +0000

    Update CoinDetailDataService.swift

--- SwiftfulCrypto/Services/CoinDetailDataService.swift
+++ SwiftfulCrypto/Services/CoinDetailDataService.swift
@@ -1,8 +1,8 @@
 //
 //  CoinDetailDataService.swift
-//  SwiftfulCrypto
+//  Crypto Tracker
 //
-//  Created by Nick Sarno on 5/11/21.
+//  Created by Hamad Ashraf on 9/9/23.
 //
 
 import Foundation
